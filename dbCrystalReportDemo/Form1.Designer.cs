f2caaa81d6b176f41e398b7a7ee2301498febf50
Md-Nahid-Hasan <romeovs0123@gmail.com>
2022-10-29 11:23:30 +0600

    db connected

---
+++ dbCrystalReportDemo/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace dbCrystalReportDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crvViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crvViewer
+            // 
+            this.crvViewer.ActiveViewIndex = -1;
+            this.crvViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crvViewer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crvViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crvViewer.Location = new System.Drawing.Point(0, 0);
+            this.crvViewer.Name = "crvViewer";
+            this.crvViewer.Size = new System.Drawing.Size(858, 432);
+            this.crvViewer.TabIndex = 0;
+            this.crvViewer.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            this.crvViewer.Load += new System.EventHandler(this.crvViewer_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 432);
+            this.Controls.Add(this.crvViewer);
+            this.Name = "Form1";
+            this.Text = "crystalReportViewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crvViewer;
+    }
+}
+
---
+++ dbCrystalReportDemo/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dbCrystalReportDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void crvViewer_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=MIS14;Initial Catalog=report;Integrated Security=True;");
+            sqlcon.Open();
+            SqlDataAdapter sqlDA = new SqlDataAdapter("Select * from Product",sqlcon);
+            DataSet dsProduct = new DataSet();
+            sqlDA.Fill(dsProduct,"Product");
+            sqlcon.Close();
+
+            SqlDataAdapter sqlDA2 = new SqlDataAdapter("Select * from Customer", sqlcon);
+            sqlDA2.Fill(dsProduct,"Customer");
+
+            CrystalReport.crptProductReport crptProduct = new CrystalReport.crptProductReport();       
+            crptProduct.Database.Tables["Product"].SetDataSource(dsProduct.Tables[0]);
+            crptProduct.Database.Tables["Customer"].SetDataSource(dsProduct.Tables[1]);
+
+            crvViewer.ReportSource = null;
+            crvViewer.ReportSource = crptProduct;
+
+        }
+    }
+}
